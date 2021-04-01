f51229726b4126f00a130ca644d389827bba60ad
Kergosien Yoann <kergosien.e1700930@etud.univ-ubs.fr>
2021-04-01 16:22:13 +0200

    TeaProcess

--- preparation/src/main/java/preparation/ServicePreparation.java
+++ preparation/src/main/java/preparation/ServicePreparation.java
@@ -1,11 +1,13 @@
 package preparation;
 
+import javax.jws.WebService;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.Scanner;
 
+@WebService(name = "ServicePreparation", targetNamespace = "http://ensibs.projet.fr/")
 public interface ServicePreparation {
 
-
+    public boolean debutPreparation(int id);
 
 }
--- preparation/src/main/java/preparation/TeaProcess.java
+++ preparation/src/main/java/preparation/TeaProcess.java
@@ -5,12 +5,34 @@ import javax.xml.ws.Response;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
+import javax.swing.Timer;
 
 public class TeaProcess extends JFrame {
 
-    public TeaProcess() {
+    private final int id;
+    private final int idUtilisateur;
+    private HashMap<String, Integer> listeThe;
+    private String etat;
+
+    private String afficheCommande;
+
+    final private JButton prepaCommence;
+    final private JButton theLivre;
+
+    private JTextArea info;
+    private int decompte = 4;
+
+    public TeaProcess(int id, int idUtilisateur, HashMap<String, Integer> listeThe, String etat) {
         super("TeaServiceAsynchronous Client");
+
+        this.id = id;
+        this.idUtilisateur = idUtilisateur;
+        this.listeThe = listeThe;
+        this.etat = etat;
+
+
 
         ServiceGestionnaire_Service gestionnaireService = new ServiceGestionnaire_Service();
         ServiceGestionnaire gestionnairePort = gestionnaireService.getGestionnairePort();
@@ -18,35 +40,48 @@ public class TeaProcess extends JFrame {
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(500, 300));
 
-        JTextArea info = new JTextArea();
-        info.setText("La commande du client a été prise en compte ?");
+        info = new JTextArea();
+        afficheCommande = "Voici la commande du client :\n";
+        for(String key : listeThe.keySet()){
+            afficheCommande +=  "        > - "+key+" x"+listeThe.get(key)+" sachets\n";
+        }
+
+        info.setText(afficheCommande);
+
+        String textInit = "\nVeuillez appuyer sur le bouton OK lorsque la préparation de la commande commence";
+
+        info.append(textInit);
+
         info.setEditable(false);
         this.getContentPane().add(BorderLayout.NORTH, info);
-
-
-        JPanel buttonPanel = new JPanel();
-
-        final JButton startButton = new JButton("OK");
-        startButton.setPreferredSize(new Dimension(75, 30));
-        buttonPanel.add(startButton);
-        this.getContentPane().add(buttonPanel);
 
         final JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
         this.getContentPane().add(BorderLayout.SOUTH, textArea);
 
-        startButton.addActionListener(new ActionListener() {
+        JPanel buttonPanel = new JPanel();
+
+        prepaCommence = new JButton("OK");
+        prepaCommence.setPreferredSize(new Dimension(75, 30));
+
+        theLivre = new JButton("OK");
+        theLivre.setPreferredSize(new Dimension(75, 30));
+        theLivre.setVisible(false);
+
+        buttonPanel.add(prepaCommence);
+        buttonPanel.add(theLivre);
+        this.getContentPane().add(buttonPanel);
+
+
+        prepaCommence.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                info.setText("Le thé à été déposé sur la zone du client ?");
+                prepaCommence.setEnabled(false);
 
                 Response res = gestionnairePort.theEnPreparationAsync(6);
-
                 textArea.setText("Envoie des informations au gestionnaire ...\n");
-
                 TheEnPreparationResponse reponse = null;
-
                 try {
                     reponse = (TheEnPreparationResponse)res.get();
                 } catch (InterruptedException interruptedException) {
@@ -55,9 +90,42 @@ public class TeaProcess extends JFrame {
                     executionException.printStackTrace();
                 }
 
-                // TODO: initialiser un service et un port.
+                if  (!res.isCancelled() && res.isDone()) {
+                    if(reponse.isReturn()){
+                        textArea.setText("Information traitée !\n");
+                    } else{
+                        textArea.setText("Information n'a pas pu être traité !\n");
+                    }
+                    info.setText(afficheCommande);
+                    info.append("\nVeuillez appuyer sur le bouton OK lorsque la préparation de la commande est terminée et que la commande est en zone de livraison");
 
-                // TODO: invoquer de manière asynchrone l'opération `addPerson` à partir du port précédent
+                    prepaCommence.setVisible(false);
+                    theLivre.setVisible(true);
+
+                } else {
+                    textArea.setText("Un problème est survenu lors de l'envoie !\n");
+                    prepaCommence.setEnabled(true);
+                }
+
+            }
+        });
+
+        theLivre.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                theLivre.setEnabled(false);
+
+                Response res = gestionnairePort.theEnPreparationAsync(6);
+                textArea.setText("Envoie des informations au gestionnaire ...\n");
+                TheEnPreparationResponse reponse = null;
+                try {
+                    reponse = (TheEnPreparationResponse)res.get();
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                } catch (ExecutionException executionException) {
+                    executionException.printStackTrace();
+                }
 
                 if  (!res.isCancelled() && res.isDone()) {
                     if(reponse.isReturn()){
@@ -65,11 +133,14 @@ public class TeaProcess extends JFrame {
                     } else{
                         textArea.setText("Information n'a pas pu être traité !\n");
                     }
-                } else {
+                    info.setText("Votre travail de préparation est terminé, la fenètre va se fermer dans :\n        > - 5\n");
+                    waitForClose();
+
+
+                }else {
                     textArea.setText("Un problème est survenu lors de l'envoie !\n");
+                    theLivre.setEnabled(true);
                 }
-
-
 
             }
         });
@@ -78,5 +149,25 @@ public class TeaProcess extends JFrame {
         this.setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+
+    public void waitForClose(){
+        Timer delay = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                info.append("        > - "+decompte+"\n");
+                decompte--;
+                waitForClose();
+            }
+        });
+        delay.setRepeats(false);
+        while(decompte>0){
+            System.out.println(decompte);
+            decompte--;
+
+
+            delay.start();
+        }
+        super.dispose();
     }
 }
