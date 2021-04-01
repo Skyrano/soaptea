e42241e875b45bd5bfabefdf1f88f1f74b9aae6e
Rameau Alistair <rameau.e1703301@etud.univ-ubs.fr>
2021-04-01 10:59:54 +0200

    Ajout Commande

---
+++ gestionnaire/src/main/java/gestionnaire/Commande.java
@@ -0,0 +1,68 @@
+package gestionnaire;
+
+import java.util.ArrayList;
+
+public class Commande {
+
+    private final int id;
+    private ArrayList<String> listeThe;
+    private ArrayList<Integer> nombreThe;
+
+    public Commande(int id, ArrayList<String> listeThe, ArrayList<Integer> nombreThe) {
+        this.id = id;
+        this.listeThe = listeThe;
+        this.nombreThe = nombreThe;
+    }
+
+    public void ajouterThe(String nomThe, int nombre) {
+        int index = listeThe.indexOf(nomThe);
+        if (index == -1 && nombre > 0) {
+            listeThe.add(nomThe);
+            nombreThe.add(1);
+        }
+        else {
+            nombreThe.set(index, nombreThe.get(index) + nombre);
+        }
+    }
+
+    public void supprimerThe(String nomThe, int nombre) {
+        int index = listeThe.indexOf(nomThe);
+        if (index != -1 && nombre >= 0) {
+            nombreThe.set(index, nombreThe.get(index) - nombre);
+            if (nombreThe.get(index) <= 0){
+                listeThe.remove(index);
+                nombreThe.remove(index);
+            }
+        }
+        else if (index != -1 && nombre == -1) {
+            listeThe.remove(index);
+            nombreThe.remove(index);
+        }
+    }
+
+    public void setNombreThe(String nomThe, int nombre) {
+        int index = listeThe.indexOf(nomThe);
+        if (index != -1 && nombre > 0) {
+            nombreThe.set(index, nombre);
+
+        }
+    }
+
+    public ArrayList<String> getListeThe() {
+        return listeThe;
+    }
+
+    public ArrayList<Integer> getNombreThe() {
+        return nombreThe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+
+
+
+}
--- gestionnaire/src/main/java/gestionnaire/ServiceGestionnaire.java
+++ gestionnaire/src/main/java/gestionnaire/ServiceGestionnaire.java
@@ -2,5 +2,9 @@ package gestionnaire;
 
 import javax.jws.WebService;
 
+@WebService(name = "ServiceGestionnaire", targetNamespace = "http://ensibs.projet.fr/")
 public interface ServiceGestionnaire {
+
+    void notifierCommande();
+
 }
