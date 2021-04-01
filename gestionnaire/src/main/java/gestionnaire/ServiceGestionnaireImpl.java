e89b4a52407a878ebf5405abd378949bbe892496
Rameau Alistair <rameau.e1703301@etud.univ-ubs.fr>
2021-04-01 11:14:28 +0200

    Ajout service wsdl suite

--- gestionnaire/src/main/java/gestionnaire/ServiceGestionnaireImpl.java
+++ gestionnaire/src/main/java/gestionnaire/ServiceGestionnaireImpl.java
@@ -1,2 +1,35 @@
-package gestionnaire;public class ServiceGestionnaireImpl {
+package gestionnaire;
+
+import javax.jws.WebService;
+import java.util.ArrayList;
+
+@WebService(endpointInterface = "gestionnaire.ServiceGestionnaire", serviceName = "ServiceGestionnaire", portName = "GestionnairePort")
+public class ServiceGestionnaireImpl implements ServiceGestionnaire {
+
+    private ArrayList<Commande> commandes;
+
+    @Override
+    public boolean notifierCommande(Commande commande) {
+        return false;
+    }
+
+    @Override
+    public boolean theEnPreparation(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean thePrepare(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean colisAuLivreur(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean theLivre(int id) {
+        return false;
+    }
 }
--- gestionnaire/src/main/java/gestionnaire/ServiceGestionnairePublisher.java
+++ gestionnaire/src/main/java/gestionnaire/ServiceGestionnairePublisher.java
@@ -1,2 +1,11 @@
-package gestionnaire;public class ServiceGestionnairePublisher {
+package gestionnaire;
+
+import javax.xml.ws.Endpoint;
+
+public class ServiceGestionnairePublisher {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Endpoint.publish("http://localhost:9991/ws/ServiceGestionnaire", new ServiceGestionnaireImpl());
+    }
+
 }
