d8313ca51dfa4cc222c477224ae0e66edfd78159
Kergosien Yoann <kergosien.e1700930@etud.univ-ubs.fr>
2021-04-03 17:25:19 +0200

    Ajout des target Déguelassssssse

---
+++ livraison/target/generated-sources/jaxws-wsimport/livraison/Paiement.java
@@ -0,0 +1,98 @@
+
+package livraison;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour paiement complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="paiement"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="arg2" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "paiement", propOrder = {
+    "arg0",
+    "arg1",
+    "arg2"
+})
+public class Paiement {
+
+    protected int arg0;
+    protected int arg1;
+    protected String arg2;
+
+    /**
+     * Obtient la valeur de la propriété arg0.
+     * 
+     */
+    public int getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Définit la valeur de la propriété arg0.
+     * 
+     */
+    public void setArg0(int value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété arg1.
+     * 
+     */
+    public int getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Définit la valeur de la propriété arg1.
+     * 
+     */
+    public void setArg1(int value) {
+        this.arg1 = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété arg2.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getArg2() {
+        return arg2;
+    }
+
+    /**
+     * Définit la valeur de la propriété arg2.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setArg2(String value) {
+        this.arg2 = value;
+    }
+
+}
