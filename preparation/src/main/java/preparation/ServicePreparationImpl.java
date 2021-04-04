a51463753fa2178185c50978f97766e6e59de357
Kergosien Yoann <kergosien.e1700930@etud.univ-ubs.fr>
2021-04-04 15:06:17 +0200

    implémentation publisher preparation

--- preparation/src/main/java/preparation/ServicePreparationImpl.java
+++ preparation/src/main/java/preparation/ServicePreparationImpl.java
@@ -15,16 +15,4 @@ public class ServicePreparationImpl implements ServicePreparation {
         return true;
     }
 
-
-
-    public static void main(String[] args) throws IOException {
-
-        HashMap<String, Integer> listeThe = new HashMap<>();
-        listeThe.put("The vert au citron", 5);
-        listeThe.put("The vert à la menthe", 2);
-        listeThe.put("The noir à la Marie-Jeanne", 25);
-
-
-        TeaProcess tea = new TeaProcess(1, 1, listeThe, "Debut");
-    }
 }
